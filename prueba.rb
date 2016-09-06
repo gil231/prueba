031ee7c08cb6a4d8e49c4189294485190b0ae985
gil <gil@MacBook-Pro-de-gil.local>
2016-09-06 15:21:28 -0600

    prueba

---
+++ prueba.rb
@@ -0,0 +1,9 @@
+print "What's your first name?"
+first_name = gets.chomp
+print "what's your last name?"
+last_name = gets.chomp
+print "what city are you from?"
+city = gets.chomp
+print "What state or providence are u from?"
+state = gets.chomp
+puts "Your name is #{first_name} #{last_name} n' you're from #{city}, #{state}"
